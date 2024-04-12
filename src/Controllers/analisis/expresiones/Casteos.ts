53237cafb3078560e5d6694259acbc0ddd063d90
helenrodas <3030360790108@ingenieria.usac.edu.gt>
2024-04-11 23:36:31 -0600

    agrego funcion de casteos

--- src/Controllers/analisis/expresiones/Casteos.ts
+++ src/Controllers/analisis/expresiones/Casteos.ts
@@ -35,43 +35,80 @@ export default class Casteos extends Instruccion {
 
         switch (this.operacion.getTipo()) {
             case tipoDato.INTEGER:
-                //console.log(Unico)
                 return this.casteo_int(Unico)
-            // case Operadores.DIFERENCIACION:
-            //     return this.diferenciacion(opIzq, opDer)
-            // case Operadores.MENOR:
-            //     return this.menor_que(opIzq, opDer)
-            // case Operadores.MENORIGUAL:
-            //     return this.menor_igual(opIzq, opDer)
-            // case Operadores.MAYOR:
-            //     return this.mayor_que(opIzq, opDer)
-            // case Operadores.MAYORIGUAL:
-            //     return this.mayor_igual(opIzq,opDer)
+            case tipoDato.DOUBLE:
+                return this.casteo_double(Unico)
+            case tipoDato.STRING:
+                return this.casteo_string(Unico)
+            case tipoDato.CHAR:
+                return this.casteo_char(Unico)
             default:
                 return new Errores("Semantico", "Casteo Invalido", this.linea, this.col)
         }
     }
 
     casteo_int(op1: any) {
-        // console.log(op1)
         let tipo1 = this.operandoUnico?.tipoDato.getTipo()
-        let tipo2 = this.operando2?.tipoDato.getTipo()
         switch (tipo1) {
             case tipoDato.DOUBLE:
                 console.log(op1)
                 this.tipoDato = new Tipo(tipoDato.INTEGER)
                 return parseInt(op1)
-            
         
             case tipoDato.CHAR:
                 this.tipoDato = new Tipo(tipoDato.INTEGER)
                 return parseInt(op1.charCodeAt(1))
+            default:
+                    return new Errores("Semantico", "No se puede castear a entero: " + op1, this.linea, this.col)
         }
     }
 
+    casteo_double(op1: any) {
+        let tipo1 = this.operandoUnico?.tipoDato.getTipo()
+        switch (tipo1) {
+            case tipoDato.INTEGER:
+                this.tipoDato = new Tipo(tipoDato.DOUBLE)
+                return parseFloat(op1)
+        
+            case tipoDato.CHAR:
+                this.tipoDato = new Tipo(tipoDato.DOUBLE)
+                return parseFloat(op1.charCodeAt(1))
+            default:
+                    return new Errores("Semantico", "No se puede castear a double: " + op1, this.linea, this.col)
+        }
+    }
+
+    casteo_string(op1: any) {
+        let tipo1 = this.operandoUnico?.tipoDato.getTipo()
+        switch (tipo1) {
+            case tipoDato.INTEGER:
+                this.tipoDato = new Tipo(tipoDato.STRING)
+                return op1.toString()
+            
+        
+            case tipoDato.DOUBLE:
+                this.tipoDato = new Tipo(tipoDato.STRING)
+                return op1.toString()
+            default:
+                    return new Errores("Semantico", "No se puede castear a string: " + op1, this.linea, this.col)
+        }
+    }
+
+    casteo_char(op1: any) {
+        let tipo1 = this.operandoUnico?.tipoDato.getTipo()
+        switch (tipo1) {
+            case tipoDato.INTEGER:
+                this.tipoDato = new Tipo(tipoDato.CHAR)
+                return String.fromCharCode(op1);
+            default:
+                    return new Errores("Semantico", "No se puede castear a char: " + op1, this.linea, this.col)
+        }
+    }
 }
 
 export enum Operadores {
     INTEGER,
-    DOUBLE
+    DOUBLE,
+    STRING,
+    CHAR
 }
